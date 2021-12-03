67dd4a63867354c90eb246ed99171dfe747a952c
Kevin Bunn <rocketlikeabass@gmail.com>
2021-12-03 07:48:14 -0700

    yo lamdas tho

--- api/routes/releases/index.js
+++ api/routes/releases/index.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 require('../../../stores/releases/model/releases');
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const app = require('../../app')
 
-mongoose.connect(process.env.MONGO_URI)
-  .catch(err => {
-    console.log(err.reason)
-  }).then(() => {
-    console.log('connected')
-  });
+// mongoose.connect(process.env.MONGO_URI)
+//   .catch(err => {
+//     console.log(err.reason)
+//   }).then(() => {
+//     console.log('connected')
+//   });
+//
+// const Index = mongoose.model('releases');
 
-const Index = mongoose.model('releases');
+app.get('/releases', jsonParser, async (req,res) => {
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.status(200).send('Releases!')
+});
 
 app.get('/releases/:id', jsonParser, async (req,res) => {
   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   const release = await Index.findOne({id: req.params.id});
-  res.send(releases)
+  res.send(release)
 });
 
 module.exports = app
