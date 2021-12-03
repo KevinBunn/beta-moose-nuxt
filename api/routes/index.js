f229b7ebc7c421f39d8b9b5960283f860fe81c61
Kevin Bunn <rocketlikeabass@gmail.com>
2021-12-03 08:03:27 -0700

    testing something

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,7 +6,7 @@ app.get('/api', (req, res) => {
   res.status(200).send('Connected!')
 })
 
-app.get('/releases', jsonParser, async (req,res) => {
+app.get('/api/releases', jsonParser, async (req,res) => {
   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(200).send('Releases!')
 });
