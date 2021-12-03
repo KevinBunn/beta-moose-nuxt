f31a76be1f7d4e3e59093bdedfe95b32dc4ad6d0
Kevin Bunn <rocketlikeabass@gmail.com>
2021-12-03 08:12:49 -0700

    testing something

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,6 @@
 const app = require('../app')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 app.get('/api', (req, res) => {
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
@@ -6,9 +8,14 @@ app.get('/api', (req, res) => {
   res.status(200).send('Connected!')
 })
 
-app.get('/api/releases', jsonParser, async (req,res) => {
+app.get('/api/releases', async (req,res) => {
   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(200).send('Releases!')
+});
+
+app.get('/api/releases/:id', jsonParser, async (req,res) => {
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.status(200).send(req.params.id)
 });
 
 export default app
